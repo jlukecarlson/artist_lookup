6d7453174f767798a98b429541ab1f45f0059c2c
Luke <jlukecarlson@gmail.com>
2012-07-24 18:18:59 -0400

    Removed unused code and files

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$("#results").hide();
-    var getArtist = function(artist){
+  var getArtist = function(artist){
     var artist = artist;
         $.ajax({
   type: 'GET',
@@ -35,68 +35,6 @@ $(document).ready(function(){
        }
    });
 });
-/*
-
-$.ajax({
-  type: 'GET',
-  url: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Frank%20Ocean&api_key=9b9ad92f8e07b96a969d8e9f944763e1",
-  data: {
-    key: "value"
-  },
-  dataType: "xml",
-  success: function(xml){
-    var result = $(xml).find('artist')  ##### .find('name')[0]; 
-    
-    name_element = result.find('name')[0];
-    var name = $(name_element).text();
-    image_element = result.find('image')[4];
-    var image = $(image_element).text();
-
-    var summary = $(result.find('bio').find('summary')[0]).text();
-    console.log(summary)  
-    $("#results").append("<h2>" + name + "</h2>" + "<img src=" + image + "> </img> <p>" + summary + "</p>");
-	}   
-	}); /* end of ajax */
-
-	/* GOOD AJAX END
-
-
-
-
-/* SHIT
-
-
-
-
-
-	$.ajax({
-    type: "GET",
-    url: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Frank%20Ocean&api_key=9b9ad92f8e07b96a969d8e9f944763e1",
-    dataType: "xml",
-    success: parseXml
-  });
-
-   var getArtist = function(){
-
-        var artist = $('#artist').val();
-        $('#results').html("<h2> Your artist is " + artist + "</h2>");
-   }
-   function parseXml(xml){
-  //find every Tutorial and print the author
-  console.log(xml);
-  	$the_pic = $(xml).find("artist").find("image").text();
-    $("#results").append(the_pic);
-  };
-		}
-   $('#search').click(getArtist);
-   $('#term').keyup(function(event){
-       if(event.keyCode == 13){
-           getArtist();
-       }
-   });
-
-}); */
-
 
 	/*url = $.get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Frank%20Ocean&api_key=9b9ad92f8e07b96a969d8e9f944763e1", xml)
 	console.log(url)
