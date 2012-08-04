4de8835de478f643fdf67970e5eb283dc8586d53
Luke <jlukecarlson@gmail.com>
2012-08-03 22:17:33 -0400

    More Error Catching

--- scripts.js
+++ scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     //$('#myModal').modal('show');
     $("#results").hide();
     $("#listen_section").hide();
-    $("#error").hide();
     var getResults = function(input) {
 	console.log("going to get results for " + input)
 	var artist = input;
@@ -24,13 +23,13 @@ $(document).ready(function(){
 	    dataType: "xml",
 	    error: function () {
 	    	$('#error').fadeIn();
-		    $('#error').html("<h3> Aw Sorry There Was An Error. The artist might not exist :O </h3>");
+		$('#error').html("<h3> Aw Sorry There Was An Error. The artist might not exist :O </h3>");
 	    },
 	    success: function(xml){
 		var status = $(xml).find('lfm');
 		console.log($(status).attr('status'));
 		if (status != 'failed'){
-			$('#error').hide();
+		    $('#error').hide();
 		    var result = $(xml).find('artist');
 		    name_element = result.find('name')[0];
 		    name = $(name_element).text();
@@ -46,7 +45,7 @@ $(document).ready(function(){
 		    $("#results").fadeIn();
 		    /* Appends all results $("#results").append("<h2>" + name + "</h2>" + "<img src=" + mega_image + "> </img> <p>" + summary + "</p>"); */
     		    $("#recent_searches").append("<li id='recent_term'> <a href='#" + name + "' /a>" + name + "</li>")
-		} 
+		}  
 	    }
 	});
     }
@@ -60,36 +59,32 @@ $(document).ready(function(){
 		key: "value"
 	    },
 	    dataType: "xml",
-	    error: function () {
-			$('#error').fadeIn();
-		    $('#error').html("<h3> Aw Sorry There Was An Error. The artist might not exist :O </h3>");
-		},
 	    success: function(xml){
 	    	var status = $(xml).find('lfm');
-			console.log($(status).attr('status'));
-			if (status != 'failed'){
-				var total_songs = $(xml).find('toptracks').attr('total');
-				if (total_songs != 0){
-					toptracks = $(xml).find('track');
-					track1 = $(toptracks[0]).find('name')[0];
-					track2 = $(toptracks[1]).find('name')[0];
-					track3 = $(toptracks[2]).find('name')[0];
-					track4 = $(toptracks[3]).find('name')[0];
-					track5 = $(toptracks[4]).find('name')[0];
-					$("#track1").html("<a href='" + $($(toptracks[0]).find('url')[0]).text() + "'>" + $(track1).text() + "</a>");
-					$("#track2").html("<a href='" + $($(toptracks[1]).find('url')[0]).text() + "'>" + $(track2).text() + "</a>");
-					$("#track3").html("<a href='" + $($(toptracks[2]).find('url')[0]).text() + "'>" + $(track3).text() + "</a>");
-					$("#track4").html("<a href='" + $($(toptracks[3]).find('url')[0]).text() + "'>" + $(track4).text() + "</a>");
-					$("#track5").html("<a href='" + $($(toptracks[4]).find('url')[0]).text() + "'>" + $(track5).text() + "</a>");
-					/*console.log($($(toptracks[1]).find('url')[0]).text()); */
-					spotifySong($(track1).text(), artist);
-				} else {
-					$('#error').html("<h3> No Songs </h3>");
-				}
+		console.log($(status).attr('status'));
+		if (status != 'failed'){
+		    var total_songs = $(xml).find('toptracks').attr('total');
+		    if (total_songs != 0){
+			toptracks = $(xml).find('track');
+			track1 = $(toptracks[0]).find('name')[0];
+			track2 = $(toptracks[1]).find('name')[0];
+			track3 = $(toptracks[2]).find('name')[0];
+			track4 = $(toptracks[3]).find('name')[0];
+			track5 = $(toptracks[4]).find('name')[0];
+			$("#track1").html("<a href='" + $($(toptracks[0]).find('url')[0]).text() + "'>" + $(track1).text() + "</a>");
+			$("#track2").html("<a href='" + $($(toptracks[1]).find('url')[0]).text() + "'>" + $(track2).text() + "</a>");
+			$("#track3").html("<a href='" + $($(toptracks[2]).find('url')[0]).text() + "'>" + $(track3).text() + "</a>");
+			$("#track4").html("<a href='" + $($(toptracks[3]).find('url')[0]).text() + "'>" + $(track4).text() + "</a>");
+			$("#track5").html("<a href='" + $($(toptracks[4]).find('url')[0]).text() + "'>" + $(track5).text() + "</a>");
+			/*console.log($($(toptracks[1]).find('url')[0]).text()); */
+			spotifySong($(track1).text(), artist);
 		    } else {
-			    $('#error').html("<h3> Aw Sorry There Was An Error. The artist might not exist :O </h3>");
+			$('#error').html("<h3> No Songs </h3>");
 		    }
+		} else {
+		    $('#error').html("<h3> Aw Sorry There Was An Error. The artist might not exist :O </h3>");
 		}
+	    }
 	});
     }
     var spotifySong = function(song, artist){
@@ -97,25 +92,29 @@ $(document).ready(function(){
 	    type: 'GET',
 	    url: "http://ws.spotify.com/search/1/track?q=" + song + " " + artist,
 	    dataType: "xml",
+	    error: function () {
+	    	$('#error').fadeIn();
+		$('#error').html("<h3> Aw Sorry There Was An Error. The artist might not exist :O </h3>");
+	    },
 	    success: function(xml){
-			top_result = $(xml).find("track")[0];
-			spotify_embed_url = "https://embed.spotify.com/?uri=" + $(top_result).attr('href')
-			$("#player_container").html('<iframe id="player" src="' + spotify_embed_url + '" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>');
-			$("#listen_section").fadeIn();
-			artist_href = $($(top_result).find('artist')).attr('href');
-			artist_code = artist_href.split('artist:');
-			/* console.log(artist_code);*/
-			$("#listen_spotify").attr("href", "http://open.spotify.com/artist/" + artist_code[1]);
-			/* console.log($(top_result)); */
+		top_result = $(xml).find("track")[0];
+		spotify_embed_url = "https://embed.spotify.com/?uri=" + $(top_result).attr('href')
+		$("#player_container").html('<iframe id="player" src="' + spotify_embed_url + '" width="300" height="80" frameborder="0" allowtransparency="true"></iframe>');
+		$("#listen_section").fadeIn();
+		artist_href = $($(top_result).find('artist')).attr('href');
+		artist_code = artist_href.split('artist:');
+		/* console.log(artist_code);*/
+		$("#listen_spotify").attr("href", "http://open.spotify.com/artist/" + artist_code[1]);
+		/* console.log($(top_result)); */
 	    }
 	});
     }
-    if(window.location.hash){ /* checks for #artist in the url and puts it through getResults to find the info 
-			       */
-	var hash = window.location.hash.substring(1);
-	console.log('the hash is' + hash);
-	getResults(hash);
-    }
+    /* if(window.location.hash){  checks for #artist in the url and puts it through getResults to find the info 
+       
+       var hash = window.location.hash.substring(1);
+       console.log('the hash is' + hash);
+       getResults(hash);
+       } */
     $('#search').click(function () {
 	getResults($('#term').val())
     });
